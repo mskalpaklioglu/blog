b3a5f38401a818d6de9efd3eab82464a05d6f18f
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-08-31 04:16:26 +0300

    .

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
+import { MatDialog } from '@angular/material/dialog';
+import { BlogDialogComponent } from './blog-dialog/blog-dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  pageSize=8;
+  page=13;
+  blogData:Array<any>=[];
+  constructor(private blogService:BlogService,public dialog:MatDialog) {}
+
+  ngOnInit():void {
+    this.blogService.getPosts().subscribe((res) =>{
+      console.log(res);
+      this.blogData = res;
+    });
+  }
+
+  openDialog(element:any,vieworupdate:any){
+    const dialogRef = this.dialog.open(BlogDialogComponent,{
+      data:{blog:element,isUpdate:vieworupdate}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.getBlogList();
+    });
+  }
+
+  getBlogList(){
+    this.blogService.getPosts().subscribe((res) =>{
+      console.log(res);
+      this.blogData = res;
+    });
+
+  }
+
+
+}
